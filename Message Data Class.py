b6b57d00d22bd373f2ad6476e7eb18bf6b50aec2
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 22:05:24 +0800

    Priority Queue class and Priority Queue

--- Message Data Class.py
+++ Message Data Class.py
@@ -11,3 +11,18 @@ class Message:
 wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
+# Priority Queue Class
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
+# Priority Queue
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
