69311d7ff5ca453e6f5e858be301f6f9f367b428
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 21:19:48 +0800

    Import Queue and Heap and Priority Queue Class

---
+++ Priority Queue Data Type.py
@@ -0,0 +1,16 @@
+# Building a Priority Queue Data Type
+
+# Import Queue and Heap
+from collections import deque
+from heapq import heappop, heappush
+
+# PriorityQueue Class
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)
