46959788ae5f8020deee492c819db2506bc6a4b4
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 16:47:24 +0800

    FIFO Queue

--- queues.py
+++ queues.py
@@ -13,3 +13,13 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+# FIFO Queue
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+print(fifo.dequeue())
+print(fifo.dequeue())
+print(fifo.dequeue())
