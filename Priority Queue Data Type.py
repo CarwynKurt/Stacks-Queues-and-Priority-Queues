4f906541ad4e1c35fc501622b1ff7e72498aa13a
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 21:29:21 +0800

    Priority Queue

--- Priority Queue Data Type.py
+++ Priority Queue Data Type.py
@@ -4,7 +4,7 @@
 from collections import deque
 from heapq import heappop, heappush
 
-# PriorityQueue Class
+# Priority Queue Class
 class PriorityQueue:
     def __init__(self):
         self._elements = []
@@ -14,3 +14,16 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)
+
+# Priority Queue
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
