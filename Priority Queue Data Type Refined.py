b5c1c7aa24394cb19ee9894e2ce8546097b26577
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 21:54:06 +0800

    Priority Queue Refined

--- Priority Queue Data Type Refined.py
+++ Priority Queue Data Type Refined.py
@@ -14,3 +14,19 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)[1]
+
+#
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
