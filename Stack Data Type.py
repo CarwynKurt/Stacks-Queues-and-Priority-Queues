4120b123695b80bdcdd1e44c9926638d89a76054
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 18:27:03 +0800

    LIFO Stack

--- Stack Data Type.py
+++ Stack Data Type.py
@@ -19,3 +19,9 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+# LIFO Stack
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
+
