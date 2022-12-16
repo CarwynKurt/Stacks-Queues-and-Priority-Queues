019bd445d7a59a61bb45abe62c4b56b9c13e4a61
CarwynKurt <carwynkurtarimado@gmail.com>
2022-12-16 16:35:50 +0800

    Impoort and Queue Class

---
+++ queues.py
@@ -0,0 +1,15 @@
+# queues.py
+
+# Import
+from collections import deque
+
+# Queue Class
+class Queue:
+    def __init__(self):
+        self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
